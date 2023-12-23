65cc0fb340c8bed2431a0bbf1e83399a709b7e50
jason torres <jasontorres585@icloud.com>
2023-12-23 06:06:29 -0500

    header and footer finished

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={100}
+            height={28} />
+        </Link>
+        <p>2023 Stuff2Do All Rights Reserved</p>
+      </div>
+    </footer>
   )
 }
 
--- components/ui/shared/Header.tsx
+++ components/ui/shared/Header.tsx
@@ -1,7 +1,9 @@
 import  Link  from 'next/link'
 import Image from 'next/image'
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { Button } from '../button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -10,7 +12,17 @@ const Header = () => {
         <Link href='/'className='w-36'>
          <Image src='/assets/images/logo.svg'width={128} height={38} alt='logo'/>
         </Link>
+        <SignedIn>
+          <nav className='md:flex-between hidden w-full max-w-xs'>
+          <NavItems /> 
+          </nav>
+          </SignedIn>
+
         <div className='flex w-32 justify-end gap-3'>
+          <SignedIn>
+            <UserButton afterSignOutUrl='/' />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className='rounded-full'size="lg">
               <Link href="/sign-in">
